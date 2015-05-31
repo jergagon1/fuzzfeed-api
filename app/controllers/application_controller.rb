eff181bfcd3a72215c86bd038da1c3b8b1f1e064
Jeremy Gagon <jergagon1@gmail.com>
2015-05-31 16:37:02 -0700

    Comment out user auth method in fuzzfriends-api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,18 @@
 class ApplicationController < ActionController::API
-  helper_method :current_user
-  before_action :current_user
+  # helper_method :current_user
+  # before_action :current_user
 
   #make a call to fuzzfriends-api to determine whether user is logged_in
   #good proof of concept of apis making requests to each other
   #but not secure
-  def current_user
-    url = "http://localhost:3000/api/v1/logged_in?email=#{params[:email]}&password_hash=#{params[:password_hash]}"
-    response = HTTParty.get(url)
-    data = JSON.parse(response.body)
-    unless data["logged_in"]
-      render json: "invalid credentials", status: 401
-    else
-      @current_user ||= data
-    end
-  end
+  # def current_user
+  #   url = "http://localhost:3000/api/v1/logged_in?email=#{params[:email]}&password_hash=#{params[:password_hash]}"
+  #   response = HTTParty.get(url)
+  #   data = JSON.parse(response.body)
+  #   unless data["logged_in"]
+  #     render json: "invalid credentials", status: 401
+  #   else
+  #     @current_user ||= data
+  #   end
+  # end
 end
